d8827801e179b5132d969a0e84727a7b30feb412
princeqjzh <11547299@qq.com>
2023-07-19 22:53:55 +0800

    Skywalking nodejs agent

--- src/main.js
+++ src/main.js
@@ -5,54 +5,100 @@ import store from './store'
 import router from './router'
 import 'lib-flexible/flexible'
 import 'vant/lib/index.css'; // 全局引入样式
+import ClientMonitor from 'skywalking-client-js';
+import Vue from 'vue'
 
 const app = createApp(App) // 创建实例
 
 // 全局过滤器
 app.config.globalProperties.$filters = {
-  prefix(url) {
-    if (url && url.startsWith('http')) {
-      return url
-    } else {
-      url = `http://localhost:28019${url}`
-      return url
+    prefix (url) {
+        if (url && url.startsWith('http')) {
+            return url
+        } else {
+            url = `http://localhost:28019${url}`
+            return url
+        }
     }
-  }
 }
 
 app.use(ActionBarButton)
-  .use(ActionBarIcon)
-  .use(ActionBar)
-  .use(Divider)
-  .use(Popup)
-  .use(Overlay)
-  .use(Loading)
-  .use(Dialog)
-  .use(Toast)
-  .use(ContactCard)
-  .use(Form)
-  .use(AddressEdit)
-  .use(AddressList)
-  .use(Field)
-  .use(CellGroup)
-  .use(Cell)
-  .use(SwipeCell)
-  .use(Icon)
-  .use(Stepper)
-  .use(Card)
-  .use(Button)
-  .use(Swipe)
-  .use(SwipeItem)
-  .use(PullRefresh)
-  .use(List)
-  .use(Tab)
-  .use(Tabs)
-  .use(SubmitBar)
-  .use(Checkbox)
-  .use(CheckboxGroup)
-  .use(Skeleton)
+    .use(ActionBarIcon)
+    .use(ActionBar)
+    .use(Divider)
+    .use(Popup)
+    .use(Overlay)
+    .use(Loading)
+    .use(Dialog)
+    .use(Toast)
+    .use(ContactCard)
+    .use(Form)
+    .use(AddressEdit)
+    .use(AddressList)
+    .use(Field)
+    .use(CellGroup)
+    .use(Cell)
+    .use(SwipeCell)
+    .use(Icon)
+    .use(Stepper)
+    .use(Card)
+    .use(Button)
+    .use(Swipe)
+    .use(SwipeItem)
+    .use(PullRefresh)
+    .use(List)
+    .use(Tab)
+    .use(Tabs)
+    .use(SubmitBar)
+    .use(Checkbox)
+    .use(CheckboxGroup)
+    .use(Skeleton)
 
 app.use(router)
 app.use(store)
 
 app.mount('#app')
+
+
+ClientMonitor.register({
+    service: 'mall-vue3',//应用名称	
+    serviceVersion: '2.0.1',//应用版本号
+    traceSDKInternal: true,//追踪sdk
+    pagePath: location.href,//当前路由地址
+    useFmp: true,
+    app: App,
+    vue: Vue, //vue实例
+});
+
+
+router.beforeEach(() => {//我选择在beforeEach中上报，其实在afterEach也可以。
+    //路由上报到skywalking
+    ClientMonitor.setPerformance({
+        pagePath: location.href,//当前路由地址。
+        useFmp: true,
+        app: App,
+        vue: Vue,
+    });//因为有些参数已经在ClientMonitor.register中注册过了所以不用填加了。
+});
+
+// Vue 报错上报到skywalking。
+Vue.config.errorHandler = (error) => {
+    console.error(error);
+    reportFrameErrors(error);
+}
+
+//监听ajax报错
+window.addEventListener('error', error => {
+    console.log("error--->", error)
+    reportFrameErrors(error);
+}, true);
+
+//上报错误信息方法
+function reportFrameErrors (error) {
+    ClientMonitor.reportFrameErrors({
+        service: '',//应用名称  
+        serviceVersion: '2.0.1',//应用版本号
+        pagePath: location.href,
+        vue: Vue,
+    }, error);
+}
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     devServer: {
-        disableHostCheck: true
+        disableHostCheck: true,
+        proxy: {
+            '/browser': {
+                target: 'http://' + 'localhost' + ':12800',//这里是路由和报错报告的代理
+                changeOrigin: true
+            },
+            '/v3': {
+                target: 'http://' + 'localhost' + ':12800',
+                changeOrigin: true//这里是追踪报告的代理
+            }
+        }
     }
 }
